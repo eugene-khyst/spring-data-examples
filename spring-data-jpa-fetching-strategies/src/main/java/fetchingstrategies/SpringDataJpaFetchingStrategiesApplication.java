9b46f3d4fb0866f1cb183dee1e53b7997cb71446
Evgeniy Khyst <evgeniy.khist@gmail.com>
2020-01-24 10:11:49 +0200

    Spring Data JPA

---
+++ spring-data-jpa-fetching-strategies/src/main/java/fetchingstrategies/SpringDataJpaFetchingStrategiesApplication.java
@@ -0,0 +1,13 @@
+package fetchingstrategies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaFetchingStrategiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaFetchingStrategiesApplication.class, args);
+	}
+
+}
